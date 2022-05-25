e35bd5e53e254ced7bb71392eb45d9f63cc16d8c
Ajayi bolarinwa <ajayibcd@gmail.com>
2022-05-25 08:22:11 -0700

    added flickr api operation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpHeaders, HttpClient, HttpParams  } from '@angular/common/http';
+// import { FlickrService } from './flickr.service';
+
 
 @Component({
   selector: 'app-root',
@@ -11,6 +13,13 @@ export class AppComponent {
   constructor(private http: HttpClient) {}
 
   title = 'weather-app';
+  items = [];
+  
+  
+
+  onSubmit(){
+    
+  }
 
   ngOnInit(): void {
     this.getWeather();
@@ -20,15 +29,18 @@ export class AppComponent {
     this.getWeather();
   }
 
+  flipImg(){
+    this.getPicture();
+  }
+
   getWeather(){
     let header = new HttpHeaders({
       'X-RapidAPI-Host': 'community-open-weather-map.p.rapidapi.com',
-      'X-RapidAPI-Key': '00ec9b22ebmsh606d8ad6fdb8db8p1651eejsnc5d86aad3b6e'
+      'X-RapidAPI-Key': '00ec9b22ebmsh606d8ad6fdb8db8p1651eejsnc5d86aad3b6e',
     });
 
   let param = {
-        q: 'London,uk',
-       
+        q: 'London,uk', 
     }
   
     this.http
@@ -36,8 +48,47 @@ export class AppComponent {
       headers: header,
       params : param
     }) 
-    .subscribe(data => {
-      console.log(data);
+    .subscribe({
+      next:(data)=>{
+        console.log(data)
+        console.log(data.clouds.all);
+        console.log(data.main.humidity);
+        console.log(data.main.temp);
+        console.log(data.main.pressure);
+        console.log(data.wind.speed);
+        console.log(data.weather[0].description);
+        this.items = data
+
+      },
+      error:(err)=>{
+        alert('Getting details unsuccessful' + err)
+      }
+      
+    });
+  }
+
+
+  imageUrl ?:` http://www.flickr.com/photos/22365685@N03/galleries/72157622254010831/format=json&jsoncallback=JSONP_CALLBACK`;
+
+  getPicture(){
+    
+
+    let headContent = new HttpHeaders({
+      'api_key': '6c5950552b30f3c31bcf1f71498f33df',
+      'id': '22342631-72157622254010831'
+    });
+
+    this.http
+    .get<any>('http://www.flickr.com/photos/22365685@N03/galleries/72157622254010831/format=json&jsoncallback=JSONP_CALLBACK',{
+      headers: headContent,
+    })
+    .subscribe({
+      next:(res)=>{
+        console.log(res)
+      },
+      error:(err)=>{
+        alert('Getting details unsuccessful' + err)
+      }  
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule,HttpClientJsonpModule } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,6 +23,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatIconModule,
     MatFormFieldModule,
     HttpClientModule,
+    HttpClientJsonpModule,
     BrowserAnimationsModule,
 
   ],
